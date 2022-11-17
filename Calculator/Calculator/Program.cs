ff5f4058ad3a75149b83b73b8a0b3d364c47f008
Strunevsky <98808799+Strunevsky@users.noreply.github.com>
2022-11-17 18:58:34 +0500

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            double a;
+            double b;
+            double otvet;
+            char operation;
+
+            Console.WriteLine("Введите A:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите операцию:");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите В:");
+            b = Convert.ToDouble(Console.ReadLine());
+        }
+                
+            
+        }
+    }
+}
